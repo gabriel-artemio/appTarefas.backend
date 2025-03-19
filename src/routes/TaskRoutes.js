4fe2628de86204f2a26b13607d91b449dbf8d110
Gabriel Artemio <43358004+gabriel-artemio@users.noreply.github.com>
2025-03-19 15:16:04 -0300

    add metodo de update task

--- src/routes/TaskRoutes.js
+++ src/routes/TaskRoutes.js
@@ -6,5 +6,6 @@ const TaskValidation = require('../middlewares/TaskValidation');
 
 //quando chegar requisição no verbo post, primeiro vai ser executado o middleware e depois a função create
 router.post('/', TaskValidation, TaskController.create);
+router.put('/:id', TaskValidation, TaskController.update);
 
 module.exports = router;
